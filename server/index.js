e0c4ede15a6880977ecb758529e7d863d10974d3
Shizawa <flores28.elodie@gmail.com>
2019-03-06 09:59:10 +0100

    JWT et mise à jour

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const MovieRoute = require('./routes/movie');
+const verifyToken = require('./middlewares/security');
 
 const app = express();
 
+app.use(verifyToken);
 app.use('/movies', MovieRoute);
 
 app.listen(3000, () => console.log('Listening'));
