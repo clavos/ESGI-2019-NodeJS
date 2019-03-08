94eb42968588b3ebc16f5cf2bebcbcce85fb28ea
Alexandre Morcrette <clavos@hotmail.fr>
2019-03-08 17:34:18 +0100

    Adding movie

--- client/src/container/Movie.js
+++ client/src/container/Movie.js
@@ -6,15 +6,24 @@ class Movie extends React.Component {
     constructor() {
         super();
         this.state = {
+            idmovie: "5c812bd26b6bfb31f357e766",
+            info: []
         };
-
+        this.getMovie(this.state.idmovie);
     }
 
-
+    getMovie(id) {
+        fetch('http://127.0.0.1:3001/movies/'+id)
+            .then(results => {
+                return results.json();
+            }).then(data => {
+            this.setState({info: data});
+        }).catch(error=>console.log(error));
+    }
     render(){
         return (
                 <div>
-                    {this.state.idMovie}
+                    {this.state.info}
                 </div>
             );
     }
